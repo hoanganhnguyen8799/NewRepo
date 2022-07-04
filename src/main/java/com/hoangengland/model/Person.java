570f0a7dabe500fde8cd784614bcb938f7993023
Hoang <test@gmail.com>
2022-07-04 21:27:14 +0700

    commit file src

---
+++ src/main/java/com/hoangengland/model/Person.java
@@ -0,0 +1,44 @@
+package com.hoangengland.model;
+
+import java.io.Serializable;
+
+public class Person implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6580506857012617507L;
+	private int age;
+	private String name,phone;
+	public Person() {
+		
+	}
+	public Person(int age, String name, String phone) {
+		super();
+		this.age = age;
+		this.name = name;
+		this.phone = phone;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	@Override
+	public String toString() {
+		return "Person [age=" + age + ", name=" + name + ", phone=" + phone + "]";
+	}
+	
+}
---
+++ src/main/java/com/hoangengland/testpai/NewAPI.java
@@ -0,0 +1,37 @@
+package com.hoangengland.testpai;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hoangengland.model.Person;
+import com.hoangengland.util.HttpUtil;
+@WebServlet(urlPatterns = "/api-demo")
+public class NewAPI extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 890102656705402807L;
+
+	/**
+	 * 
+	 */
+
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		resp.setContentType("application/json");
+		
+		Person p = HttpUtil.of(req.getReader()).toModel(Person.class);
+		System.out.println(p);
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.writeValue(resp.getOutputStream(), p);
+	}
+}
